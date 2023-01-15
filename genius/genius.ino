90794b060a2a220af1c19b33af5bcf0669680165
nnnnadia <tristaonadia@gmail.com>
2023-01-15 17:03:52 -0300

    feat: red led is now blinking

--- genius/genius.ino
+++ genius/genius.ino
@@ -3,10 +3,12 @@ int redLed = 2;
 void setup() {
   // put your setup code here, to run once:
   pinMode(redLed, OUTPUT);
-  digitalWrite(redLed, 1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(redLed, 1);
+  delay(1000);
+  digitalWrite(redLed, 0);
+  delay(500);
 }
