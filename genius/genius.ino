2651c29576faca1937c2c305e453f0fa816ac052
nnnnadia <tristaonadia@gmail.com>
2023-01-15 17:14:50 -0300

    feat: example using Serial to debug output

--- genius/genius.ino
+++ genius/genius.ino
@@ -3,12 +3,15 @@ int redLed = 2;
 void setup() {
   // put your setup code here, to run once:
   pinMode(redLed, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println(redLed);
   digitalWrite(redLed, HIGH);
   delay(1000);
   digitalWrite(redLed, LOW);
   delay(500);
+  redLed++;
 }
