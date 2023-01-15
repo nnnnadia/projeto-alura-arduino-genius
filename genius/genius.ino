f6d1712a05e9bef1926a270a64e70e8a7121579f
nnnnadia <tristaonadia@gmail.com>
2023-01-15 17:20:53 -0300

    feat: make blinkLed a separete function

--- genius/genius.ino
+++ genius/genius.ino
@@ -1,17 +1,23 @@
-int redLed = 2;
+// int redLed = 2;
+#define RED_LED 2
+#define ONE_SECOND 1000
+#define HALF_SECOND 500
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(redLed, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(redLed);
-  digitalWrite(redLed, HIGH);
-  delay(1000);
-  digitalWrite(redLed, LOW);
-  delay(500);
-  redLed++;
+  Serial.println(RED_LED);
+  blinkLed();
+}
+
+void blinkLed() {
+  digitalWrite(RED_LED, HIGH);
+  delay(ONE_SECOND);
+  digitalWrite(RED_LED, LOW);
+  delay(HALF_SECOND);
 }
